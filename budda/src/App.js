bb904db35ec12a151ccaa747b8604ea37cfda67b
IMTHEBESTKOREANMAN <kim246464@naver.com>
2024-11-16 01:04:18 +0900

    111

--- budda/src/App.js
+++ budda/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./router/MainPage";
 import RecipePage from "./router/RecipePage";
@@ -6,23 +6,30 @@ import LoginPage from "./router/LoginPage";
 import NotFound from "./router/NotFound";
 import SignUpPage from "./router/SignUpPage";
 import Logincom from "./router/LoginCom";
+
+import { GoogleOAuthProvider } from "@react-oauth/google";
 const dbConnect = require("./config/dbConnect");
 
-dbConnect();
-
 function App() {
+  // 데이터베이스 연결 (한 번만 실행)
+  useEffect(() => {
+    dbConnect();
+  }, []);
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/budda" element={<MainPage />} />
-        <Route path="/recipe" element={<RecipePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="*" element={<NotFound />} />
-        <Route path="/login/signup" element={<SignUpPage />} />
-        <Route path="/login/com" element={<Logincom />} />
-      </Routes>
-    </Router>
+    // <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE}>
+      <Router>
+        <Routes>
+          <Route path="/budda" element={<MainPage />} />
+          <Route path="/recipe" element={<RecipePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/login/signup" element={<SignUpPage />} />
+          <Route path="/login/com" element={<Logincom />} />         
+
+        </Routes>
+      </Router>
+
   );
 }
 
