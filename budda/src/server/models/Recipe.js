4ed00f4924999c4088bb2d5a5e07bf7c6af62e96
mmk622 <mmk622@naver.com>
2024-11-30 22:17:54 +0900

    코드 꼬임 해결
    레시피 저장 시 uploadTime 필드 추가

--- budda/src/server/models/Recipe.js
+++ budda/src/server/models/Recipe.js
@@ -26,6 +26,7 @@ const recipeSchema = new mongoose.Schema({
   userId: { type: String, required: true }, // 사용자 ID 필드 추가
   likes: { type: Number, default: 0 }, // 좋아요 수
   likedBy: { type: [String], default: [] }, // 좋아요를 누른 사용자의 ID 목록
+  uploadTime: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model("Recipe", recipeSchema);
